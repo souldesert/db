035212388285ab195ad81ba2ba5951e53814665d
Alex <y915@yandex.ru>
2017-01-07 00:27:19 +0300

    Создание таблиц и столбцов в ней, выбор активной таблицы, добавление записей в таблицу

---
+++ main.py
@@ -0,0 +1,90 @@
+import os
+import glob
+
+print("========= Welcome to my database, ver 1.0 =========")
+
+current_table = ""
+
+
+def create_table():
+    print("==================== New table ====================")
+    try:
+        table_name = input("Enter table name: ")
+        open(table_name + ".txt", "x")
+        global current_table
+        current_table = table_name
+        print("New table successfully created!")
+    except FileExistsError:
+        print("Table already exists!")
+
+
+def set_columns():
+    print("================== Set column(s) ==================")
+    print("Enter column names, separated by semicolon")
+    print("For example: one;two;three")
+    columns = input("Enter column names: ")
+    print("You have entered: ")
+    for name in columns.split(";"):
+        print(name)
+    table = open(current_table + ".txt", "w")
+    table.write(columns)
+
+
+def add_record():
+    table = open(current_table + ".txt", "r")
+    columns = table.readline().split(";")
+    table.close()
+    table = open(current_table + ".txt", "a")
+    record = ""
+    for column in columns:
+        element = input(column.rstrip('\n') + ": ")
+        # element = element.rstrip()
+        record += element
+        record += ";"
+    table.write("\n" + record.rstrip(';'))
+    table.close()
+
+
+def select_table():
+    print("================== Select table ===================")
+    tables = glob.glob("*.txt")
+    for table in tables:
+        print(table.replace(".txt", ""))
+    selected = input("Select table: ")
+    if selected + ".txt" in tables:
+        global current_table
+        current_table = selected
+        print("Table \"" + current_table + "\" have been selected")
+    else:
+        print("No such table!")
+
+while True:
+    print("Choose option:")
+    print("1. Create new table")
+    print("2. Set column(s) in current table")
+    print("3. Add record to table")
+    print("4. Select table")
+    print("0. Exit")
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        create_table()
+    elif choice == 2:
+        if current_table != "":
+            set_columns()
+        else:
+            print("No selected table!")
+    elif choice == 3:
+        if current_table != "":
+            if os.stat(current_table + ".txt").st_size != 0:
+                add_record()
+            else:
+                print("There is no columns in table!")
+        else:
+            print("No selected table!")
+    elif choice == 4:
+        select_table()
+    elif choice == 0:
+        print("Goodbye!")
+        break
+    else:
+        print("Incorrect value!")
